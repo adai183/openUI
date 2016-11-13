a185a131f3786dfd8afc20cd1a46971741718950
Ryan Collins <rccproductions@mac.com>
2016-11-13 16:34:48 -0500

    feat: RUn eslint installation

--- README.md
+++ README.md
@@ -7,5 +7,7 @@ At the moment, this project is a work in progress.  We have combined primitive c
 ## Integrations
 - [Grommet UX](https://grommet.github.io/) - This library utilizes the fantastic Grommet UX Framework as a starting place. All grommet components are included and can be imported from the OpenUI library.
 
+NOTE: to properly use this lib, you will need to import the base grommet stylesheets.  See `./source/examples/` for an example of the usage.
+
 ## Status
 This is a brand new project and is totally experimental.  As it matures, we will release more information, including documentation.  Stay tuned!
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import cssModules from 'react-css-modules';
 import styles from './index.module.css';
 import {
@@ -6,31 +6,76 @@ import {
   Title,
   Header,
   Anchor,
+  Sidebar,
+  Split,
 } from 'components';
 
-const Navbar = ({
-  logo,
-  links,
-}) => (
-  <nav className={styles.nav}>
-    <Header justify="between">
+class Navbar extends Component {
+  constructor() {
+    super();
+    this.renderTitle = this.renderTitle.bind(this);
+    this.renderMobile = this.renderMobile.bind(this);
+  }
+  renderTitle() {
+    const {
+      logo,
+    } = this.props;
+    return (
       <Title>
         <img className={styles.logo} src={logo} alt="logo"/>
       </Title>
-      <Menu direction="row" align="center" responsive={false}>
-        {links.map((item, i) =>
-          <Anchor key={i} href={item.url}>
-            {item.name}
-          </Anchor>
-        )}
-      </Menu>
-    </Header>
-  </nav>
-);
+    );
+  }
+  renderMobile() {
+    const {
+      links,
+      onToggleMobileNav,
+    } = this.props;
+    return (
+      <Sidebar size="medium" colorIndex="neutral-1" fixed seperator="right">
+        {this.renderTitle()}
+        <Menu primary>
+          {links.map((item, i) =>
+            <Anchor key={i} href={item.url} onClick={() => onToggleMobileNav(i)}>
+              {item.name}
+            </Anchor>
+          )}
+        </Menu>
+      </Sidebar>
+    )
+  }
+  render() {
+    const {
+      logo,
+      links,
+      isResponsive,
+    } = this.props;
+    return (
+      <nav className={styles.nav}>
+        <Header justify="between">
+          {this.renderTitle()}
+          <Menu direction="row" align="center" responsive={false}>
+            {links.map((item, i) =>
+              <Anchor key={i} href={item.url}>
+                {item.name}
+              </Anchor>
+            )}
+          </Menu>
+        </Header>
+      </nav>
+    )
+  }
+}
 
 Navbar.propTypes = {
   logo: PropTypes.string,
   links: PropTypes.array.isRequired,
+  isResponsive: PropTypes.bool.isRequired,
+  onToggleMobileNav: PropTypes.func
+};
+
+Navbar.defaultProps = {
+  isResponsive: true,
 };
 
 export default cssModules(Navbar, styles);
